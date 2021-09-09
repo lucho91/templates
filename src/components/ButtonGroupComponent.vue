cbed81e8d95733308f8e0349b087e04a73a32e31
INTEREDES <joseperdomo@jcinnova.com>
2021-09-09 07:24:11 -0500

    Avances

---
+++ src/components/ButtonGroupComponent.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div>
+    <button-component buttonText="Desde grupo button"></button-component>
+    <other-button-component buttonText="Other Desde grupo button"></other-button-component>
+    </div>
+</template>
+<script>
+
+import ButtonComponent from '../components/ButtonComponent';
+import OtherButtonComponent from '../components/OtherButtonComponent';
+export default {
+  name: 'ButtonGroupComponent',
+  components: {
+    ButtonComponent,
+    OtherButtonComponent
+  }
+}
+</script>
